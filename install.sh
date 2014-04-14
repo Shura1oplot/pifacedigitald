08b8dd266666458dd25b80c79b32b2faef019f50
Shura1oplot <s0meuser@yandex.ru>
2014-04-14 21:20:04 +0400

    add debian build rules

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir -p /etc/pifacedigitald/event.d
-cp event /etc/pifacedigitald/event
-cp pifacedigitald /usr/local/bin/pifacedigitald
-cp pifacedigital-cli /usr/local/bin/pifacedigital-cli
